925a21bd2552370e24e20257c8cea4cfbe0eb4e6
apcs2 <apcs2@p-162mba-25.d214.org>
2018-04-06 09:46:23 -0500

    stuff

---
+++ README.md
@@ -0,0 +1,21 @@
+![alt text](https://i.imgur.com/OvHkZbA.png "HHCS Logo")
+# Hexadecimal Heredity Coding String
+[![Build Status](https://travis-ci.org/HoogleyBoogley/hexadecimal-heredity-coding-string.svg?branch=master)](https://travis-ci.org/HoogleyBoogley/hexadecimal-heredity-coding-string)
+[![Coverage Status](https://coveralls.io/repos/github/HoogleyBoogley/hexadecimal-heredity-coding-string/badge.svg?branch=master)](https://coveralls.io/github/HoogleyBoogley/hexadecimal-heredity-coding-string?branch=master)
+[![MIT Licence](https://badges.frapsoft.com/os/mit/mit.png?v=103)](https://opensource.org/licenses/mit-license.php)
+[![npm version](https://badge.fury.io/js/hexadecimal-heredity-coding-string.svg)](https://badge.fury.io/js/hexadecimal-heredity-coding-string)
+[![Open Source Love](https://badges.frapsoft.com/os/v1/open-source.png?v=103)](https://github.com/ellerbrock/open-source-badges/)
+
+Open source digital genetics.
+
+
+## Introduction
+Welcome to Hexadecimal Heredity Coding String, digital genetics. So what exactly is HHCS? HHCS is an open source javascript project for virtual genetics and heredity.
+## Installation
+
+  `npm install @hoogleyboogley/hexadecimal-heredity-coding-string`
+
+
+## Contributing
+
+In lieu of a formal style guide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code.
---
+++ src/main.js
@@ -0,0 +1,162 @@
+/*
+ *	Hexadecimal Heredity Coding String
+ *	Open source digital genetics simulation
+ *	https://github.com/HoogleyBoogley/hexadecimal-heredity-coding-string
+ *
+ *	Copyright (c) 2018 Danny Tran - https://hoogleyboogley.com
+ *	Acceptable use under the MIT license
+ */
+
+class HB_HHCS {
+
+	constructor(quantity = 2, traits = 20, delay = 1000, runTimes = 1, maxValues= []) {
+		// Set values
+		this.quantity = quantity;
+		this.traits = traits;
+		this.delay = delay;
+		this.heredityCodingStrings = {};
+
+		// Create and log the initial strings
+		for (var i = 0; i < quantity; i++) {
+			var str = this.create(i, traits, maxValues).join(', ');
+			console.log("[" + str + "]");
+			console.log("");
+			console.log("");
+		}
+
+		for (var i = 0; i < runTimes; i++) {
+			setTimeout(this.reproduce, delay * i, "", "", this.heredityCodingStrings, this.traits);
+		}
+	}
+
+	/*	====================
+	 *	Create initial strings
+	 *	==================== */
+	create(quantity, traits, maxValues = []) {
+		// Temp array
+		var heredityString = [];
+
+		// Create the IDs for the initial strings
+		var numOfHeredityStrings = Object.keys(this.heredityCodingStrings).length;
+		var StringID = "0x" + numOfHeredityStrings.toString(16);
+
+		// Set the initial strings' gender
+		var gender = "0x" + Math.floor(Math.random() * 2);
+		heredityString.push(gender);
+
+		// If trait lengths isn't set, then set it to 9999
+		if (!maxValues.length) {
+			for (var i = 0; i < traits; i++) {
+				maxValues.push(9999);
+			}
+		}
+
+		for (var i = 0; i < traits; i++) {
+			// Generate initial random values
+			var randInt = Math.floor(Math.random() * Math.floor(maxValues[i] + 1));
+			var hexInt = "0x" + randInt.toString(16);
+			heredityString.push(hexInt);
+		}
+
+		// Create a new object in the dictionary
+		this.heredityCodingStrings[StringID] = heredityString;
+
+		// Log the id
+		console.log("\x1b[36m", "Creating subject " + StringID + "!", "\x1b[0m");
+
+		// Return the newly created string
+		return heredityString;
+	}
+
+	/*	====================
+	 *	Create child string
+	 *	==================== */
+	reproduce(maleID = "", femaleID = "", hhcsArr, numOfTraits) {
+
+		var maleID = maleID;
+		var femaleID = femaleID;
+
+		if (maleID == "") {
+			maleID = "0x" + Math.floor(Math.random() * Object.keys(hhcsArr).length).toString(16);
+			console.log("FatherID: " + maleID);
+		}
+
+		if (femaleID == "") {
+			femaleID = "0x" + Math.floor(Math.random() * Object.keys(hhcsArr).length).toString(16);
+			console.log("MotherID: " + femaleID);
+		}
+
+		// Set id values for the mother and father strings
+		var father = hhcsArr[maleID];
+		var mother = hhcsArr[femaleID];
+
+		// Create the child string id
+		var numOfHeredityStrings = Object.keys(hhcsArr).length;
+		var StringID = "0x" + numOfHeredityStrings.toString(16);
+
+		// Create array to contain child string information
+		var newChild = [];
+
+		// Make sure the first parameters are different
+		if (father[0] != mother[0]) {
+
+			// Set the child string's gender
+			newChild.push("0x" + Math.floor(Math.random() * 2));
+
+			// Create the trait values
+			for (var i = 1; i < numOfTraits + 1; i++) {
+				// Decide to Inherit mother's or father's trait
+				if (Math.floor(Math.random() * 2) == 1) {
+					// Inherit father's trait
+					newChild.push(father[i]);
+				} else {
+					// Inherit mother's trait
+					newChild.push(mother[i]);
+				}
+			}
+
+			// Log the new child string
+			var str = newChild.join(', ');
+			console.log("\x1b[36m", "Creating subject " + StringID + "(Child of " + maleID + " and " + femaleID + ")!", "\x1b[0m");
+			console.log("[" + str + "]");
+			console.log("");
+
+			// Reproduction successful, return true value
+			return true;
+		} else {
+			// Reproduction unsuccessful, return false value
+			console.log("\x1b[31m", "Unable to reproduce!", "\x1b[0m");
+			console.log("");
+			return false;
+		}
+
+	}
+
+}
+
+/*	====================
+ *	Testing
+ *	==================== */
+
+var HHCS = new HB_HHCS(4, 20, 1000, 10, [
+	255,
+	255,
+	40,
+	5,
+	255,
+	9999,
+	255,
+	25,
+	255,
+	2420,
+	255,
+	255,
+	65,
+	255,
+	98,
+	255,
+	123,
+	26,
+	255,
+	15,
+]);
